8cd36cd6b257361cd4078b6e52a584cc51319766
SSSNathan <nathan@scientificss.co.uk>
2025-01-27 17:45:38 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css'; // We'll create this file later for Tailwind CSS
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
