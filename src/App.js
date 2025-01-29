b0400b889e6da7ee3ba9a35c629557964789c49c
SSSNathan <nathan@scientificss.co.uk>
2025-01-29 21:56:42 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Card, CardContent } from "./components/ui/card";
-import { Button } from "./components/ui/button";
+import { Button } from "./components/ui/button"; 
 
 // Sorted elements data
 const elementsData = [
